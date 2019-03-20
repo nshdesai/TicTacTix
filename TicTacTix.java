5fcc757453718e70dd1190a02b87a4cf195a41b3
Nishkrit Desai <nishkritdesai10@gmail.com>
2019-03-20 11:06:22 -0400

    Completed requirement #1

--- TicTacTix.java
+++ TicTacTix.java
@@ -1,4 +1,17 @@
+/**
+ * class for the Tic-Tactix game. This class will instantiate the
+ * game board and will handle all moves made (both by the user and the computer).
+ * It also retains the state of the game board and determines if a player had won the
+ * game
+ *
+ * @author Nishkrit Desai
+ * @version 20th March 2019
+ *
+ */
 public class TicTacTix {
+    /**
+     * Default constructor for the TicTacTix game board
+     */
     public TicTacTix () {
 
     }
--- TicTacTixTest.java
+++ TicTacTixTest.java
@@ -1,6 +1,56 @@
-public class TicTacTixTest {
-    public static void main(String[] args) {
+import java.util.Scanner;
+import java.util.InputMismatchException;
 
+/**
+ * A menu driven, terminal based implemnentation of the Tic-Tactix game
+ *
+ * @@author Nishkrit Desai
+ * @@version 20th March 2019
+ * @@see TicTacTix
+ *
+ */
+public class TicTacTixTest {
+    private static final Scanner input = new Scanner(System.in);
+    private static final int GOING_FIRST_PROMPT = 0;
+
+    public static void main(String[] args) {
+        boolean goingFirst = goingFirst();
     }
-    
+
+    /**
+     * Returns a boolean that says whether the user wants to go first or not.
+     * Uses input validation to ensure that the user enters one of (y/Y/n/N)
+     * @return boolean Is the user going first?
+     */
+    private static boolean goingFirst() {
+        String response = "";
+
+        while (true) {
+            printPrompt(GOING_FIRST_PROMPT);
+            response = input.nextLine();
+            System.out.println("Your response was: " + response);
+
+            if (response.toLowerCase().equals("y")) {
+                return true;
+            }
+            else if (response.toLowerCase().equals("n")) {
+                return false;
+            }
+        }
+    }
+
+    /**
+     * Prints the prompt for user input for each instance where the user input is
+     * required. Uses a set of private constants to check which prompt is required.
+     * @param promptType Which prompt is to be printed?
+     */
+    private static void printPrompt(int promptType) {
+        switch (promptType) {
+            case GOING_FIRST_PROMPT:
+                System.out.print("Would you like to go first (y/Y/n/N): ");
+                break;
+            default:
+                System.out.print("Good-bye!");
+        }
+    }
 }
